df4e258f694d7232d57759f96bbf2b88430adbef
Akshay Nagpal <akshay2626@gmail.com>
2014-05-23 17:31:37 +0530

    updated code

--- display.php
+++ display.php
@@ -1,24 +1,68 @@
 <?php
+session_start();
 require 'connect.php';
+//$email='';
+//$password='';
 
-$name = $_GET['name'];
-$phone = $_GET['phone'];
-$email=$_GET['email'];
-$photopath=$_GET['photopath'];
-$date=$_GET['dob'];
-
-echo "NAME: ";
-echo $name;
-echo "<br/>";
-echo "PHONE: ";
-echo $phone;
-echo "<br/>";
-echo "EMAIL:";
-echo $email;
-echo "<br/>";
-echo "DATE: ";
-echo $date;
-echo "<br/>";
-echo "YOUR PICTURE";
-echo "<img src=\"$photopath\" />";
+//$email = $_POST['loginEmail'];
+//$password = $_POST['loginPassword'];
+				if(isset($_POST['loginEmail']) && isset($_POST['loginPassword']))  //when fileds are filled in login form
+				{
+				  $email = $_POST['loginEmail'];
+				  $password = $_POST['loginPassword'];
+				  $passCheck = "CALL checkPass('$email')";
+				  $passCheckResult = mysqli_query($con,$passCheck) or die(mysqli_error($con));
+				  $passCheckArray= mysqli_fetch_array($passCheckResult);
+				  mysqli_free_result($passCheckResult);
+				  mysqli_close($con);
+				  
+				  //connecting again
+				  $con= mysqli_connect('localhost','root') or die("cannot connect");
+				  
+				  mysqli_select_db($con,'user_db') or die("cannot select!");
+				  
+				  if (mysqli_connect_errno())
+				  {
+				  echo "Failed to connect to MySQL: ".mysqli_connect_error(); 
+				  }
+				  //connection code ends
+				  
+								  if($passCheckArray['password'] == $password)  //if password matches
+								  {
+									$displayQuery = "SELECT * FROM user_details WHERE email='$email'";
+									$displayResult = mysqli_query($con,$displayQuery) or die(mysqli_error($con));
+									$displayArray = mysqli_fetch_array($displayResult);
+									mysqli_free_result($displayResult);
+									echo "NAME: ";
+									echo $displayArray['name'];
+									echo "<br/>";
+									echo "PHONE: ";
+									echo $displayArray['phone'];
+									echo "<br/>";
+									echo "EMAIL:";
+									echo $displayArray['email'];
+									echo "<br/>";
+									echo "DATE: ";
+									echo $displayArray['date'];
+									echo "<br/>";
+									echo "YOUR PICTURE";
+									echo $displayArray['photo'];
+									echo '<img src="'.$displayArray['photo'].'"/>';
+									echo "<a href=index.html>Log Out</a>";
+								  }
+								  
+								  else  //password mismatch
+								  {
+									echo "Wrong Password !";
+									echo "<a href=index.html>Try Again</a>";
+								  }
+				}
+				
+				else //redirect to homepage for login
+				{
+				  echo "Please login to view this page.!Redirecting to homepage...";
+				
+		   
+				  header("refresh:3;url=index.html");
+				}
 ?>
--- form_submit.php
+++ form_submit.php
@@ -1,16 +1,21 @@
 <?php
 session_start();
 require 'connect.php';
+include 'pass_gen.php';
 include_once 'securimage/securimage.php';
 
 $phone=0;
 $photoAccept=0;
 $securimage = new Securimage();
+$photoPath = "uploads/" . $_FILES["uploadPhoto"]['name'];
+echo $photoPath;
+echo $_FILES['uploadPhoto']['tmp_name'];
+
+
+/* image code
+
 move_uploaded_file($_FILES["uploadPhoto"]["name"], "" . $_FILES["uploadPhoto"]["tmp_name"]);
 
-$photoDetails = getimagesize($_FILES["uploadPhoto"]["tmp_name"]);
-$photoWidth= $photoDetails[0];
-$photoHeight= $photoDetails[1];
 $allowedExts = array("gif", "jpeg", "jpg", "png");
 //$allowedExts = array("gif","png");
 $temp = explode(".", $_FILES["uploadPhoto"]["name"]);
@@ -18,60 +23,124 @@ $extension = end($temp);
 
 if ((($_FILES["uploadPhoto"]["type"]=="image/gif")||($_FILES["uploadPhoto"]["type"] == "image/jpeg")||($_FILES["uploadPhoto"]["type"]=="image/jpg")||($_FILES["uploadPhoto"]["type"]=="image/png"))&& in_array($extension, $allowedExts))
 {
+   $photoDetails = getimagesize($_FILES["uploadPhoto"]["tmp_name"]);
+	$photoWidth= $photoDetails[0];
+   $photoHeight= $photoDetails[1];
 	 	if($_FILES['uploadPhoto']['size'] <= 1048576) 
   		{
   	    	if($photoWidth<=300 && $photoHeight<=300)
 			{
 				$photoAccept=1 ;
-				move_uploaded_file($_FILES["uploadPhoto"]["tmp_name"], "uploads/" . $_FILES["uploadPhoto"]["name"]);
-				$photoPath = "uploads/" . $_FILES["uploadPhoto"]["name"];
+				//move_uploaded_file($_FILES["uploadPhoto"]["tmp_name"], "uploads/" . $_FILES["uploadPhoto"]["name"]);
+				//$photoPath = "uploads/" . $_FILES["uploadPhoto"]["name"];
 			}
-			else  echo "Photo not less than 300 X 300! Please upload file <=300 X 300 only!";
+			else  echo "Photo greater than 300 X 300! Please upload file <=300 X 300 only!";
   		}
-		else echo "File greater than 1MB! Please upload file <= 1 MB only!"	;
+		else echo "Image greater than 1MB!!"	;
 		
   
 }
-else echo "Image Format not accepted! Please try again!";
+else echo "Image Format not accepted!!";
+
+
+*/
+
   
-if($photoAccept==1)
-{
-if ($securimage->check($_POST['captcha_code']) == true)  // checking captcha code and image attributes
-{
 
-// function to prevent user from running sql code into form.
+		if ($securimage->check($_POST['captcha_code']) == true)  // checking captcha code and image attributes (2)
+		{
 
-function make_safe($variable) { 
-    $variable = mysql_real_escape_string(trim($variable));
-    return $variable;
-}
-;
-$name = make_safe($_POST['name']);
-$email = make_safe($_POST['email']);
-$phone = make_safe($_POST['phone']);
-$date = make_safe($_POST['dob']);
-$photoPath=make_safe($photoPath);
+		// function to prevent user from running sql code into form.
+		function make_safe($variable) { 
+    	$variable = mysql_real_escape_string(trim($variable));
+    	return $variable;
+		}
 
-//echo $phone;
+		$name = make_safe($_POST['name']);
+		$email = make_safe($_POST['email']);
+		$phone = make_safe($_POST['phone']);
+		$date = make_safe($_POST['dob']);
+		$photoPath = make_safe($photoPath);
+		$password = rand_string(8);
+		
+		
+		$uniqueResult = mysqli_query($con,"SELECT * FROM user_details WHERE email='$email'") or die(mysqli_error($con));
+		
+					if(mysqli_num_rows($uniqueResult)==0)  // if email unique
+					{
+						    
+							//photo check///////////////////////////////////////
+							
+							
+							move_uploaded_file($_FILES['uploadPhoto']['name'], "" . $_FILES['uploadPhoto']['tmp_name']);
 
-$q1 = "CALL InsertValues('$name','$email','$phone','$date','$photoPath','abc')";
-$res = mysqli_query($con,$q1) or die(mysqli_error($con));
+							$allowedExts = array("gif", "jpeg", "jpg", "png");
+							$temp = explode(".", $_FILES["uploadPhoto"]["name"]);
+							$extension = end($temp);
+							
+							if ((($_FILES["uploadPhoto"]["type"]=="image/gif")||($_FILES["uploadPhoto"]["type"] == "image/jpeg")||($_FILES["uploadPhoto"]["type"]=="image/jpg")||($_FILES[				 							"uploadPhoto"]["type"]=="image/png"))&& in_array($extension, $allowedExts))
+							{
+							   $photoDetails = getimagesize($_FILES['uploadPhoto']['tmp_name']);
+								$photoWidth= $photoDetails[0];
+							   $photoHeight= $photoDetails[1];
+									if($_FILES['uploadPhoto']['size'] <= 1048576) 
+									{
+										if($photoWidth<=300 && $photoHeight<=300)
+										{
+											$photoAccept=1 ;
+											move_uploaded_file($_FILES["uploadPhoto"]["tmp_name"],$photoPath);
+							
+											$q1 = "CALL InsertValues('$name','$email','$phone','$date','$photoPath','$password')";
+											$res = mysqli_query($con,$q1) or die(mysqli_error($con));
 
-if($res)
-{
-echo "success! values inserted!" ;
-echo "Click <a href=\"display.php?name=$name&phone=$phone&email=$email&photopath=$photoPath&dob=$date\"> here </a> to display your profile!" ;
-}
-else
-echo "Error ";
-}
-else   //captcha mismatch
-{
-echo "The security code entered was incorrect.<br /><br />";
-echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
-exit;
-}
-}
-else echo "Please upload image again!";
+													  if($res)
+													  {
+													  echo "Registration successful!" ;
+										  
+													  /*
+													  MAIL CODE
+													  $mailSubject="Welcome".$name;
+													  $mailBody= "You have been successfully registered!Your password is".$password;	
+													  if(mail($email,$mailSubject,$mailBody))
+													  {
+													  echo "An Email has been sent to your email address ".$email;
+													  }
+													  */
+										  
+													  echo "Click <a href=\"index.html\"> here </a> to login!";
+													  }
+									  
+													  else
+													  { echo "Unable to register! Please try again! "; }
+							
+						
+							
+										}
+										else  echo "Photo greater than 300 X 300! Please upload file <=300 X 300 only!";
+									}
+									else echo "Image greater than 1MB!!"	;
+									
+							  
+							}
+							else echo "Image Format not accepted!!";
+	
+							
+							//photo check///////////////////////////////////////
+							
+							
+					}
+					else
+					{
+					echo "Email id already exists in database!Please try with a different email address!";
+					echo "Click <a href=\"index.html\"> here </a> to go back and try again!";
+					}
+		}
+		else   //captcha mismatch (2)
+		{
+		echo "The security code entered was incorrect.<br /><br />";
+		echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
+		exit;
+		}
+
 
 ?>
